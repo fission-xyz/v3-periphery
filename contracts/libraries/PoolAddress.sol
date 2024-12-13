a5634fcee823139d637d8b2de796dae8fcbea166
Curtis M. Humphrey, Ph.D <curtis@fission.xyz>
2024-12-13 10:14:53 -0600

    fix: changed computeAddress to use lookup to avoid bytecode changing issue

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >=0.5.0;
 
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol';
+
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
-
     /// @notice The identifying key of the pool
     struct PoolKey {
         address token0;
@@ -26,23 +26,15 @@ library PoolAddress {
         return PoolKey({token0: tokenA, token1: tokenB, fee: fee});
     }
 
-    /// @notice Deterministically computes the pool address given the factory and PoolKey
+    /// @notice Uses the factory's getPool to get the pool address given the factory and PoolKey
     /// @param factory The Uniswap V3 factory contract address
     /// @param key The PoolKey
     /// @return pool The contract address of the V3 pool
-    function computeAddress(address factory, PoolKey memory key) internal pure returns (address pool) {
+    function computeAddress(address factory, PoolKey memory key) internal view returns (address pool) {
         require(key.token0 < key.token1);
-        pool = address(
-            uint256(
-                keccak256(
-                    abi.encodePacked(
-                        hex'ff',
-                        factory,
-                        keccak256(abi.encode(key.token0, key.token1, key.fee)),
-                        POOL_INIT_CODE_HASH
-                    )
-                )
-            )
-        );
+
+        IUniswapV3Factory v3factory = IUniswapV3Factory(factory);
+
+        pool = v3factory.getPool(key.token0, key.token1, key.fee);
     }
 }
